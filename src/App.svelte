014decc917f65de61e14fa9112d0fd6f630ba7aa
Jesse Andrews <jesse@planet.com>
2020-12-07 18:14:18 -0800

    first pass at add/move
    - dropping a link onto the page will open it
      (in an iframe/box)
    - you can move it, or close it (or drag a contained link from it)

--- src/App.svelte
+++ src/App.svelte
@@ -1,34 +1,86 @@
 <script>
-  export let name;
+  import Box from "./Box.svelte";
+  const id = () => "_" + Math.random().toString(36).substr(2, 9);
+
+  const randomNumberInRange = (min, max) =>
+    Math.ceil(Math.random() * (max - min) + min);
+
+  const newItem = (src) => ({
+    src,
+    x: randomNumberInRange(0, 500),
+    y: randomNumberInRange(0, 500),
+    w: 200,
+    h: 200,
+    id: id(),
+  });
+
+  let items = [];
+
+  function add(src, props) {
+    const item = {
+      ...newItem(src || "http://localhost:8000/0.html"),
+      ...(props || {}),
+    };
+
+    items = [...items, item];
+  }
+
+  const move = (id, x, y) => {
+    items = items.map((item) => (item.id === id ? { ...item, x, y } : item));
+  };
+
+  const remove = (item) => {
+    items = items.filter((value) => value.id !== item.id);
+  };
+
+  const moveStart = (event, item) => {
+    var style = window.getComputedStyle(event.target, null);
+    event.dataTransfer.setData(
+      "text/xy",
+      parseInt(style.getPropertyValue("left"), 10) -
+        event.clientX +
+        "," +
+        (parseInt(style.getPropertyValue("top"), 10) - event.clientY) +
+        "," +
+        item.id
+    );
+  };
+
+  const allowDrop = (ev) => {
+    ev.preventDefault();
+  };
+
+  const drop = (ev) => {
+    ev.preventDefault();
+
+    var url = ev.dataTransfer.getData("text/uri-list");
+    if (url) {
+      return add(url, { x: ev.clientX, y: ev.clientY });
+    }
+    const xyid = ev.dataTransfer.getData("text/xy");
+    if (xyid) {
+      const [x, y, id] = xyid.split(",");
+      move(id, parseInt(x, 10) + ev.clientX, parseInt(y, 10) + ev.clientY);
+    }
+  };
 </script>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
+  #page {
+    background: #eee;
+    position: fixed;
+    width: 100%;
+    height: 100%;
   }
 </style>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the
-    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
-</main>
+<div id="page" on:drop={drop} on:dragover={allowDrop}>
+  <button on:click={() => add()}>Add (random size)</button>
+
+  {#each items as item, i (item.id)}
+    <Box
+      bind:item
+      handleDelete={() => remove(item)}
+      handleMove={(e) => moveStart(e, item)} />
+  {/each}
+</div>
---
+++ src/Box.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let item;
+  export let handleDelete;
+  export let handleMove;
+</script>
+
+<style>
+  #close {
+    cursor: pointer;
+    position: absolute;
+    right: 5px;
+    top: 3px;
+  }
+
+  iframe {
+    margin-top: 20px;
+    width: 100%;
+    height: 100%;
+  }
+  .box {
+    position: absolute;
+    background: #ccc;
+  }
+  .controls {
+    background: #888;
+  }
+</style>
+
+<div
+  class="box"
+  style="width: {item.width}px; height: {item.height}px; top: {item.y}px; left: {item.x}px"
+  draggable={true}
+  on:dragstart={handleMove}>
+  <div class="controls">
+    move ...
+    <span id="close" on:click={(e) => handleDelete(e)}>[x]</span>
+  </div>
+  <iframe src={item.src} />
+</div>
